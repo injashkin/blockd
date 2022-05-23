2b81929f25a634ccb4f693f67b34768167aee54e
injashkin <injashkin@gmail.com>
2022-05-23 20:00:12 +0300

    Настройка среды разработки на Webpack
    Проект настроен на разработку с помощью инструментов Pug и Sass

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('path');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true,
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    watchFiles: ['./src', './images'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.pug'),
+      filename: 'index.html',
+    }),
+  ],
+};
